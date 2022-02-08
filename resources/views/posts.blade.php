3822f376d7a8c6115a9145d875fab20b25b082d9
Hayden <haydos.shaw@gmail.com>
2022-02-08 17:59:08 +1300

    Added subtitle2

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
     <h1>The Blog</h1>
-    <p>I've added this subtitle to test the auto-deploy functionality of Forge.</p>
+    <p>I've added this subtitle to test the auto-deploy functionality of Forge. Wow. Forge worth it.</p>
     <hr>
     @foreach ($posts as $post)
         <p>
