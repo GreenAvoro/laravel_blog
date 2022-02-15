50ed440e7d10963f7b4ad1eeb8a55e09ef06a3ac
GreenAvoro <haydos.shaw@gmail.com>
2022-02-15 16:20:31 +1300

    fixed post-card

--- resources/views/components/post_card.blade.php
+++ resources/views/components/post_card.blade.php
@@ -1,10 +1,12 @@
 
 
-<p class="bg-gray-200 p-6">
+<div class="bg-gray-200 p-6 m-3">
     <a href="/post/{{$post->slug}}">
         <h2>{{$post->title}}</h2>
     </a>
     <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
     <p>{{$post->excerpt}}</p>
-    <p style="font-size: .9rem; color: #222">Written by: <a href="user/{{$post->author->id}}">{{$post->author->name}}</a></p>
-</p>
+    <p style="font-size: .9rem; color: #222">
+        Written by: <a href="user/{{$post->author->id}}">{{$post->author->name}}</a>
+    </p>
+</div>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,7 +1,7 @@
 <x-layout >
     <x-slot name="content">
         @foreach ($posts as $post)
-            <x-post_card :post="$post"></x-post_card>
+            <x-post_card :post="$post" />
         @endforeach
     </x-slot>
 </x-layout>
