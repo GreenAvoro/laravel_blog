fd51f1dc71d08767707922a30b67d1975ee7606e
Hayden <haydos.shaw@gmail.com>
2022-03-02 12:22:11 +1300

    removed dropdiown

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,7 +8,7 @@
             @else
                 <h1 class="text-2xl mb-5"><span class="text-teal-300 mr-1">/</span>News</h1>
             @endif
-            <x-dropdown></x-dropdown>
+            
         </div>
         @foreach ($posts as $post)
             <x-post_card :post="$post" />
